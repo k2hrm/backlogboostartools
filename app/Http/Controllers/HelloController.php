0c56cd06aa21f975d9c1a76561395dadc7bb6b7a
katsuyuki.harima <katsuyuki.harima@s-cubism.jp>
2021-02-07 19:46:21 +0900

    P52Request及びResponse

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,20 +2,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke()
-    {
-return <<<EOF
+public function index(Request $request,Response $response) {
+    $html = <<<EOF
 <html>
 <head>
-<title>Hello</title>
+<title>Hello/Index</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 {font-size:120pt;}
+</style>
 </head>
 <body>
-これはシングルアクションコントローラーのアクションです。
+<h1>Hello</h1>
+<h3>Request</h3>
+<pre>{$request}</pre>
+<h3>response</h3>
+<pre>{$response}</pre>
 </body>
 </html>
 EOF;
-    }
+   $response->setContent($html);
+   return $response;
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,3 @@ Route::get('/', function () {
 });
 
 Route::get('hello','App\Http\Controllers\HelloController@index');
-Route::get('hello/other','App\Http\Controllers\HelloController@other');
