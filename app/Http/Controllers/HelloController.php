216de2582d88f30cf24007af0f7b28f30a38a658
katsuyuki.harima <katsuyuki.harima@s-cubism.jp>
2021-02-15 21:02:37 +0900

    P62 コントローラーでテンプレートを使う

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,26 +6,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-public function index(Request $request,Response $response) {
-    $html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size:120pt;}
-</style>
-</head>
-<body>
-<h1>Hello</h1>
-<h3>Request</h3>
-<pre>{$request}</pre>
-<h3>response</h3>
-<pre>{$response}</pre>
-</body>
-</html>
-EOF;
-   $response->setContent($html);
-   return $response;
-}
+  public function index(Request $request,Response $response) {
+    return view('hello.index');
+  }
 }
