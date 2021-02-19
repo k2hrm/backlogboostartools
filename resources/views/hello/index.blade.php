c49bcefbccb4e17a52830faab8a410d2e4d2fefe
katsuyuki.harima <katsuyuki.harima@s-cubism.jp>
2021-02-19 22:35:03 +0900

    P.75 @ifを利用する

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -4,7 +4,11 @@
 </head>
 <body>
   <h1>Blade/Index</h1>
-  <p>{{$msg}}</p>
+  @if ($msg != '')
+  <p>こんにちは、{{$msg}}さん。</p>
+  @else
+  <p>何か書いてください</p>
+  @endif
   <form method="POST" action="/laravel/public/hello">
   {{ csrf_field() }}
   <input type="text" name="msg">
