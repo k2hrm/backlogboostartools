2791b2d5e8037973f2dbfefd266d7ca68fecd659
katsuyuki.harima <katsuyuki.harima@s-cubism.jp>
2021-02-24 00:27:43 +0900

    P.77 @issetで変数定義をチェックする

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
   public function index() {
-    $data =[
-        'msg'=>'お名前を入力してください。',
-    ];
-    return view('hello.index',['msg'=>'']);
+    return view('hello.index');
   }
 
   public function post(Request $request)
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -4,11 +4,11 @@
 </head>
 <body>
   <h1>Blade/Index</h1>
-  @if ($msg != '')
+  @isset ($msg)
   <p>こんにちは、{{$msg}}さん。</p>
   @else
   <p>何か書いてください</p>
-  @endif
+  @endisset
   <form method="POST" action="/laravel/public/hello">
   {{ csrf_field() }}
   <input type="text" name="msg">
