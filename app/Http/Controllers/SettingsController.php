bb92b159f3da729e09c0d6d5091ca3fa7de8b92b
Katsuyuki Harima <katsu.home@gmail.com>
2021-04-07 05:38:28 +0900

    Replace setting method from model to controller

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Setting;
+use Validator;
 
 class SettingsController extends Controller
 {
@@ -30,5 +31,32 @@ class SettingsController extends Controller
         public function edit(Request $request)
     {
         return view('settings/edit');
+    }
+
+    public function store(Request $request){
+            //dd($request);
+        $validator = Validator::make($request->all(), [
+        'hostname' => 'required|max:255',
+    ]);
+        //バリデーション:エラー 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+        // Eloquentモデル（登録処理）
+    $settings = Setting::find($request->user_id);
+    if($settings === null) {
+        $settings = new Setting;
+    }
+    $settings->user_id = $request->user_id;
+    $settings->hostname = $request->hostname;
+    $settings->api_key = $request->api_key;
+    $settings->proj_key = $request->proj_key;
+    $settings->bl_user_id = $request->bl_user_id;
+    $settings->save(); 
+    return redirect('/settings');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,31 +20,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/settings/edit', function(Request $request) {
-    //dd($request);
-        $validator = Validator::make($request->all(), [
-        'hostname' => 'required|max:255',
-    ]);
-        //バリデーション:エラー 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-        // Eloquentモデル（登録処理）
-    $settings = Setting::find($request->user_id);
-    if($settings === null) {
-        $settings = new Setting;
-    }
-    $settings->user_id = $request->user_id;
-    $settings->hostname = $request->hostname;
-    $settings->api_key = $request->api_key;
-    $settings->proj_key = $request->proj_key;
-    $settings->proj_key = $request->bl_user_id;
-    $settings->save(); 
-    return redirect('/settings');
-});
+Route::post('/settings/edit', 'App\Http\Controllers\SettingsController@store');
 
 Route::get('/settings', function () {
     $settings = Setting::orderBy('created_at','asc')->get();    
@@ -57,7 +33,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/settings/edit', [App\Http\Controllers\SettingsController::class, 'edit'])->name('settings-edit');
 Route::get('/aggregate', function() {
-   $settings = Setting::orderBy('created_at','asc')->get();    
+   $settings = Setting::find('created_at','asc')->get();    
     return view('/aggregate/index',[
         'settings'=>$settings
     ]);
