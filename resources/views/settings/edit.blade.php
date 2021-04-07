c7d9ee882578e75bb568df4bd0957bd1d63c673a
Katsuyuki Harima <katsu.home@gmail.com>
2021-04-08 04:23:21 +0900

    Edit detail

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -2,56 +2,54 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="card-body">
+          @if (session('status'))
+          <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+          </div>
+          @endif
+          <h1>Backlog基本設定　編集</h1>
+          <p><a href="{{ url('settings/') }}">戻る</a></p>
 
-                    {{ __('You are logged in!') }}
-                    <h1>編集</h1>
-                    <p><a href="{{ url('settings/') }}">戻る</a></p>
+          @include('common.errors')
+          <form action="{{ url('settings/edit') }}" method="POST" class="form-horizontal">
+            @csrf
+            <div class="form-group">
+              <div class="col-sm-6">
+                <table>
+                  <tr>
+                    <th>ユーザーID</th>
+                    <td><input type="text" name="bl_user_id" class="form-control"></td>
+                  </tr>
 
-                    @include('common.errors')
-                    <form action="{{ url('settings/edit') }}" method="POST" class="form-horizontal">
-                    @csrf
-                    <div class="form-group">
-                      <div class="col-sm-6">
-                      <table>
-                        <tr>
-                          <th>ユーザーID</th><td><input type="text" name="bl_user_id" class="form-control"></td>
-                        </tr>
+                  <tr>
+                    <th>ホスト名</th>
+                    <td><input type="text" name="hostname" class="form-control"></td>
+                  </tr>
+                  <tr>
+                    <th>APIキー</th>
+                    <td><input type="text" name="api_key" class="form-control"></td>
+                  </tr>
+                </table>
+                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
 
-                        <tr>
-                          <th>ホスト名</th><td><input type="text" name="hostname" class="form-control"></td>
-                        </tr>
-                        <tr>
-                          <th>APIキー</th><td><input type="text" name="api_key" class="form-control"></td>
-                        </tr>
-                        <tr>
-                          <th>プロジェクトキー</th><td><input type="text" name="proj_key" class="form-control"></td>
-                        </tr>
-                      </table>
-                      <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="col-sm-offset-3 col-sm-6">
-                        <button type="submit" class="btn btn-primary">
-                        保存
-                        </button>
-                      </div>
-                    </div>
-                    </form>
+              </div>
             </div>
+            <div class="form-group">
+              <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-primary">
+                  保存
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+  @endsection
--- resources/views/settings/list.blade.php
+++ resources/views/settings/list.blade.php
@@ -9,33 +9,20 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
                     <h1>設定</h1>
-                    <p><a href="settings/edit">編集</a></p>
                     <p><a href="{{ url('/') }}">戻る</a></p>
-                    <!--table>
-                      <tr>
-                      <th>APIキー</th><td>gjklsdfgdf</td>
-                      </tr>
-                      <tr>
-                      <th>ホスト名</th><td>gjklsdfgdf</td>
-                      </tr>
-                      <tr>
-                      <th>プロジェクトキー</th><td>gjklsdfgdf</td>
-                      </tr>
-                    </table-->
-                    {{Auth::user()->id}}
-                    @foreach($settings as $setting)
-                    <li>ホスト名 : {{$setting->hostname}}</li>
-                    <li>APIキー : {{$setting->api_key}}</li>
-                    <li>プロジェクトキー : {{$setting->proj_key}}</li>
-                    <li>ユーザーID : {{$setting->bl_user_id}}</li>
-                    @endforeach
+                    <h2>Backlog基本設定 <a href="settings/edit">編集</a></h2>
+                    <ul>
+                        @foreach($settings as $setting)
+                        <li>ホスト名 : {{$setting->hostname}}</li>
+                        <li>APIキー : {{$setting->api_key}}</li>
+                        <li>ユーザーID : {{$setting->bl_user_id}}</li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
