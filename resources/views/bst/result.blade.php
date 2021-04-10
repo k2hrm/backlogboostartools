82ddcbf000621d4713770bedfefb0babfbd428c8
Katsuyuki Harima <katsu.home@gmail.com>
2021-04-10 10:42:02 +0900

    Change ul element into table eleement

--- resources/views/bst/result.blade.php
+++ resources/views/bst/result.blade.php
@@ -4,16 +4,35 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 mb-4 mt-2">
-            <h1>{{$from_date}}から{{$to_date}}までの{{$proj_key}}の実績時間集計結果</h1>
-            <table border="1">
-                @foreach($issueKeyAndHoursArrs as $value)
+            <h1>{{$proj_key}}の実績時間集計結果</h1>
+            <ul>
+                <li>
+                    集計期間 : {{$from_date}}から{{$to_date}}まで
+                </li>
+            </ul>
+            <table>
+                @foreach($issueKeyAndHoursArrs as $issueKeyAndHoursArr)
+                @if($loop->first)
                 <tr>
-                    @foreach($value as $v)
+                    @foreach($issueKeyAndHoursArr as $v)
+                    <th>{{$v}}</th>
+                    @endforeach
+                </tr>
+                @else
+                <tr>
+                    @foreach($issueKeyAndHoursArr as $v)
                     <td>{{$v}}</td>
                     @endforeach
                 </tr>
+                @endif
                 @endforeach
             </table>
+            <section>
+                独自の集計結果にカスタマイズしたい！<br />
+                ファイル形式で出力したい！<br />
+                などのお問い合わせはメールkatsu.home@gmail.comまで<br />
+                お気軽にお問い合わせください
+            </section>
             <p><a href="../bst/">戻る</a></p>
         </div>
     </div>
