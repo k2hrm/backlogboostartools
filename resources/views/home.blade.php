fd3af4f6f80793bf55df13eac351e13adf8f2a79
Katsuyuki Harima <katsu.home@gmail.com>
2021-04-14 09:26:01 +0900

    Improve toppage layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,34 @@
 <div class="container toppage">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>Backlog Booster Tools</h1>
-            <img src="{{ url('/img/booster.png') }}" class="topimg" />
-            <h2>期間集計ツール<span>Period Aggregation Tool</span></h2>
-            <p>プロジェクトごとの指定期間内での作業実績時間を集計します。 <a href=" aggregate/">>>Go</a></p>
-            <h2>一斉送信ツール<span>Send At Once Tool</span></h2>
-            <p>同じ投稿をバックログの複数プロジェクトに一斉送信できるツールです。 <a href="send/">>>Go</a></p>
-            <h2>日報作成ツール<span>Daily Report Generate Tool</span></h2>
-            <p>Backlogの活動から、ユーザーの日報を生成するツールです。 <a href="dailyreport/">>>Go</a></p>
-            <h2>設定<span>Settings</span></h2>
-            <p>各種設定情報を登録します。<a href="settings/">>>Go</a></p>
-            <h2>マニュアル<span>Manuals</span></h2>
-            <p>各ツールの操作マニュアルです。 <a href="manuals/">>>Go</a></p>
-            <h2>お問い合わせ<span>Contanct</span></h2>
-            <p>ご要望や不具合報告などはこちらまでお願い致します。<a href="contact/">>>Go</a></p>
+            <div class="top-title-area">
+                <h1>Backlog Booster Tools</h1>
+                </a> <img src="{{ url('/img/booster.png') }}" class="topimg" />
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href=" aggregate/">期間集計ツール<span>Period Aggregation Tool</span></a></h2>
+                <p>プロジェクトごとの指定期間内での作業実績時間を集計します。</p>
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href="send/">一斉送信ツール<span>Send Group Tool</span></a></h2>
+                <p>同じ投稿をバックログの複数プロジェクトに一斉送信できるツールです。</p>
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href="dailyreport/">日報作成ツール<span>Daily Report Generate Tool</span></a></h2>
+                <p>Backlogの活動から、ユーザーの日報を生成するツールです。 </p>
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href="settings/">設定<span>Settings</span></a></h2>
+                <p>各種設定情報を登録します。</p>
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href="manuals/">マニュアル<span>Manuals</span></a></h2>
+                <p>各ツールの操作マニュアルです。</p>
+            </div>
+            <div class="tool-list-wrapper">
+                <h2><a href="contact/">お問い合わせ<span>Contanct</span></a></h2>
+                <p>ご要望や不具合報告などはこちらまでお願い致します。</p>
+            </div>
         </div>
     </div>
 </div>
