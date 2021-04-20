48d64f9b1a96f6fde076eca77ac056de729b03ba
Katsuyuki Harima <katsu.home@gmail.com>
2021-04-20 20:06:07 +0900

    Remove unnecessery divs

--- resources/views/settings/nomember.blade.php
+++ resources/views/settings/nomember.blade.php
@@ -3,24 +3,16 @@
 @section('description', 'Backlog基本設定を定義します。')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    <h1>設定</h1>
-                    <p>メンバーのみの機能です。</p>
-                    <p><a href="register">メンバー登録する</a></p>
-                    <p>すでにメンバー登録している方は<a href="login">ログイン</a>してください。</p>
-                </div>
-            </div>
+    <div class="col-md-8">
+        @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
+        @endif
+        <h1>設定</h1>
+        <p>メンバーのみの機能です。</p>
+        <p><a href="register">メンバー登録する</a></p>
+        <p>すでにメンバー登録している方は<a href="login">ログイン</a>してください。</p>
     </div>
 </div>
 @endsection
